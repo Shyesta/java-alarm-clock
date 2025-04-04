934bafd107722312ebdaed652575f8653c1dc6b9
Sage Bain <bain.sage@student.greenriver.edu>
2025-04-03 18:43:51 -0700

    Updated AlarmClockMain to format a time for an alarm clock

--- src/AlarmClockMain.java
+++ src/AlarmClockMain.java
@@ -1,4 +1,30 @@
 package src;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
 public class AlarmClockMain {
+    public static void main(String[] args) {
+        //JAVA ALARM CLOCK
+
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime = null;
+        while(alarmTime == null) {
+            try {
+                System.out.print("Enter an alarm time (HH:MM:SS): ");
+                String inputTime = scanner.nextLine();
+
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println("Alarm set for: " + alarmTime);
+            }
+            catch (DateTimeParseException e) {
+                System.out.println("Invalid alarm format. Please use HH:MM:SS");
+            }
+        }
+
+        scanner.close();
+    }
 }
