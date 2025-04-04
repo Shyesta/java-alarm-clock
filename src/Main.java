45d224acbd2aca57a5e6cc45ef898d5a6dbda8f2
Sage Bain <bain.sage@student.greenriver.edu>
2025-04-03 18:36:23 -0700

    Altered the game to be ping pong, using multithreaded programming

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        MyRunnable run = new MyRunnable();
-        Thread secondThread = new Thread(run);
-        secondThread.setDaemon(true);
-        secondThread.start();
+        Thread thread1 = new Thread(new MyRunnable("PING"));
+        Thread thread2 = new Thread(new MyRunnable("PONG"));
 
-        System.out.println("You have 10 seconds to enter your name");
+        System.out.println("GAME START!");
 
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello " + name);
+        thread1.setDaemon(true);
+        thread2.setDaemon(true);
+        thread1.start();
+        thread2.start();
 
-        scanner.close();
+        try {
+            thread1.join();
+            thread2.join();
+        }
+        catch (InterruptedException e) {
+            System.out.println("Main thread was interrupted");
+        }
+
+        System.out.println("GAME OVER!");
     }
 }
--- src/MyRunnable.java
+++ src/MyRunnable.java
@@ -1,11 +1,18 @@
 package src;
 
 public class MyRunnable implements Runnable {
+
+    private final String text;
+
+    public MyRunnable(String text) {
+        this.text = text;
+    }
     @Override
     public void run() {
         for(int i = 1; i <= 10; i++) {
             try {
                 Thread.sleep(1000);
+                System.out.println(text);
             }
             catch(InterruptedException e) {
                 System.out.println("Thread was interrupted");
